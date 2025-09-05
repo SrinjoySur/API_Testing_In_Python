bbd54dcb8663ea5b8b697052206f6e7702e06f79
Srinjoy Sur <srinjoy_sur@epam.com>
2025-09-05 17:27:32 +0530

    chore: updated GetPostsById.py

--- venvrequest/src/getrequests/GetPostsById.py
+++ venvrequest/src/getrequests/GetPostsById.py
@@ -1,13 +1,16 @@
 import requests
-def GetPostsById():
+import pytest
+@pytest.mark.parametrize("header",[
+    "application/json; charset=utf-8"
+])
+def test_GetPostsById(header):
     baseUri="https://jsonplaceholder.typicode.com"
     response=requests.get(baseUri+"/posts/1")
     assert response.ok
-    assert response.headers["Content-Type"] == "application/json; charset=utf-8"
+    assert response.headers["Content-Type"] == header
     data =response.json()
     assert isinstance(data,dict)
     assert "id" in data
     assert "title" in data
     assert "body" in data
-    
-GetPostsById()
+     
