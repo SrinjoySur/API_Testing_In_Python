e713c3989a76f4136835bed50f80f4c615f6723e
Srinjoy Sur <srinjoy_sur@epam.com>
2025-09-05 16:55:21 +0530

    chore: updated all posts tests with assertions

--- venvrequest/src/deleterequests/Posts.py
+++ venvrequest/src/deleterequests/Posts.py
@@ -1,5 +1,4 @@
 import requests
 baseUri="https://jsonplaceholder.typicode.com"
-response=requests.delete(baseUri+"/posts/11111111")
-print(response.status_code)
-print(response.text)
+response=requests.delete(baseUri+"/posts/1")
+assert response.ok
--- venvrequest/src/putrequests/Posts.py
+++ venvrequest/src/putrequests/Posts.py
@@ -1,9 +1,8 @@
 import requests
 import json
-file=open("venvrequest\payloads\payload2.json","r") # type: ignore
+file=open("venvrequest/payloads/payload2.json","r") 
 input=file.read()
 payload=json.loads(input)
 baseUri="https://jsonplaceholder.typicode.com"
 response=requests.put(baseUri+"/posts/1",data=payload)
-print(response.status_code)
-print(response.text)
+assert response.ok
